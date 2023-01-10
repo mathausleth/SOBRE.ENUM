ab66b95b8b78959f5f8d23bd2a35f6d58f6104f2
Mathaus <109297995+mathausleth@users.noreply.github.com>
2023-01-10 23:25:44 +0100

    Initial commit: SOBRE.ENUM /**  * Version : 3  * Revision : 11/01/2023  * Auteur : Mathaus Leth  */

---
+++ SOBRE.js
@@ -0,0 +1,61 @@
+//###START### File - SOBRE.js //###->###
+/**
+*#############################################################################*
+*##                                                                         ##*
+*##     ~   {                    SOBRE.JS                         }   ~     ##*
+*##                                                                         ##*
+*#############################################################################*
+**/
+/**
+ * Version : 3
+ * Revision : 11/01/2023
+ * Auteur : Mathaus Leth
+ */
+//#!Debut!#
+//
+//--!--
+const SOBRE = {};
+//--!--
+//  ##  LES CONSTANTES  ##  //
+const T = true;
+const F = false;
+const N = null;
+const U = undefined;
+const V = '';
+//--!--
+//  ##  LA DEFINITION  ##   //
+(function () {
+//--!--
+// -> Les Namespaces (MAJUSCULES et ANGLAIS):
+//#region
+SOBRE.ENUM = {};
+//#endregion
+//--!--
+// -> Les Fonctions (UpperCamelCase et FRANCAIS):
+//#region
+//#endregion
+//--!--
+// -> Les Fonctions 'booléennes' (lowerCamelCase et ANGLAIS):
+//#region
+//#endregion
+//--!--
+// -> Les Fonctions 'this' (UpperCamelCase et ANGLAIS):
+//#region
+//#endregion
+//--!--
+// -> Les Objets (MAJUSCULES et FRANCAIS):
+//#region
+//#endregion
+//--!--
+// -> Les Variables (minuscules et FRANCAIS):
+//#region
+//#endregion
+//--!--
+// -> Les Variables 'require' (minuscules et ANGLAIS):
+//#region
+//#endregion
+//--!--
+}());
+//--!--
+//#!Fin!#
+//###<-### File - SOBRE.js //###END###
---
+++ exemples/enumeration.js
@@ -0,0 +1,8 @@
+const { SOBRE } = require('../SOBRE.js.js');
+
+console.log(SOBRE.ENUM.CRYPTO.GET('sha512'));
+console.log(SOBRE.ENUM.ELEMENT.GET(SOBRE.ENUM.ELEMENT.BODY));
+console.log(SOBRE.ENUM.DISPLAY.GET(SOBRE.ENUM.ELEMENT.DEFAUT));
+console.log(SOBRE.ENUM.UNIT.DECLARE(15, 'em'));
+console.log(SOBRE.ENUM.COLOR.GET('grey'));
+console.log(SOBRE.ENUM.COLOR.GETALPHA());
